64049957487cbf608a480a88fddbb8ef367b76d3
zhangjincheng <zhangjincheng@huidr.com>
2019-06-05 13:40:02 +0800

    redis插件

--- src/Redis/Redis.php
+++ src/Redis/Redis.php
@@ -27,7 +27,7 @@ class Redis implements DBInterface
 
     public function __call($name, $arguments)
     {
-        $this->_lastQuery = '';
+        $this->_lastQuery = $name;
         return $this->execute($name, function () use ($name, $arguments) {
             return call_user_func_array([$this->_redis, $name], $arguments);
         });
